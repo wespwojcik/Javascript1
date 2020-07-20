c54b5bb2408fc992f799f327ce5820dbaaddf871
wespwojcik <67170488+wespwojcik@users.noreply.github.com>
2020-07-20 16:40:26 -0700

    javascript assignment1

---
+++ js/Basic_JavaScript_1.js
@@ -0,0 +1,33 @@
+
+ 
+var A = " this is a string \"this is backslashing\" he said, "; // uses the backslashing expression to preserve part of the strings quotation marks//
+
+var B = " hello my name is " + "wesley wojcik "; //concactnating the strings and asinging the value to variable B//
+
+var c = A + B; //asigns variable c the value of variable A and variable B being concactnated 
+
+document.write(c);
+
+var j; // declares a variable//
+
+j = 55 + 55; // asigns varriable two integers being added 
+window.alert(j); // displays the value of j by using alert//
+
+
+
+var D = "bob" , G = "jim" , F = "paige" , Y = "steph"; // makes a bunch of variables in one line of code //
+
+
+
+
+var D = D.fontcolor("green");//Javascript changes the font color to green //
+document.write(D);//variable D is displayed//
+
+var G = G.fontcolor("purple");//javascript changes the font color to purple//
+document.write(G);//variable G is displayed
+
+var F = F.fontcolor("red");//javascript changes the font color to red//
+document.write(F);//variable F is displayed//
+
+var Y = Y.fontcolor("black");
+document.write(Y);//variable Y is displayed//
