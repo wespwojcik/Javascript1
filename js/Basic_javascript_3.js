83f4910c19cf8ec77ee03e34dbcfd2447716e94d
wespwojcik <67170488+wespwojcik@users.noreply.github.com>
2020-07-21 08:27:50 -0700

    Javascript_asigment

---
+++ js/Basic_javascript_3.js
@@ -0,0 +1,45 @@
+function myFunkadd()
+{
+
+    var example =  2 + 2;
+    document.getElementById("demo").innerHTML = "2 + 2 = " + example;
+}
+
+function myFunkminus()
+{
+
+    var example2 = 2 - 2;
+    document.getElementById("demo2").innerHTML = " 2 - 2 = " + example2; 
+}
+
+function myFunkmult()
+{
+
+    var example3 = 2 * 2;
+    document.getElementById("demo3").innerHTML = " 2 * 2 = " + example3;
+}
+
+function myFunkdiv()
+{
+
+    var example4 = 2 / 2;
+    document.getElementById("demo4").innerHTML = " 2 / 2 = " + example4;
+}
+
+function myFunkinc()
+{
+
+    var example5 = 75;
+    example ++;
+    example --;
+    document.getElementById("demo5").innerHTML = example5;
+}
+
+function myfunkRand()
+{
+
+    var x = Math.random();
+    document.getElementById("demo6").innerHTML = x;
+
+
+}
