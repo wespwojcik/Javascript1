713f8268d2cd6d152be1f0b70b2c62ef38adc910
wespwojcik <67170488+wespwojcik@users.noreply.github.com>
2020-07-21 14:25:29 -0700

    Logic assignment

---
+++ js/Basic_JavaScript_5.js
@@ -0,0 +1,54 @@
+//function myFuction()
+{
+    //document.getElementById("example").innerHTML = isNaN(x);
+
+  ///  document.write(10 > 2); 
+
+  //  var x = "dude";
+    //var z = "10";
+    //document.write(10 != 8 && 5 == 5);
+    
+   // document.getElementById("example2").innerHTML = !( 5> 15);
+
+    //console.log(false); //displays result in browser console
+
+    
+}
+function datatypeDisplay() //type of display
+{
+    var x = "hey there";
+    document.write(typeof x);
+}
+
+function combine()//combine string
+{
+    document.write("10" + 5);
+}
+
+function logic() //Or logic
+{
+    document.write(70 == 70 || 32 < 105);
+}
+
+function logic2() // And logic
+{
+document.write(5 == 5 && 5 < 2)
+}
+
+function logic3() // Not logic
+{
+
+document.write(20 <! 40);
+
+}
+
+
+
+function logic4() //triple equal sign 
+{
+
+    document.write(10 === "10");
+
+}
+
+
